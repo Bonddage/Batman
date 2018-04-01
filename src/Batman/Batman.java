213bca931df43a191bfb3046e5b167321e4de0a3
Bonddagr <bonddage15@yandex.ru>
2018-04-01 16:41:09 +0400

    Batman!

--- src/Batman/Batman.java
+++ src/Batman/Batman.java
@@ -2,6 +2,6 @@ package Batman;
 
 public class Batman {
     public static void main(String[] args) {
-        System.out.println("I'am Batman");
+        System.out.println("I'am Batman!");
     }
 }
